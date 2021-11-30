b4f35ea569a4a9a85aed5725cab806919a88064d
Nersesk <Knersesk@gmail.com>
2021-11-30 18:53:42 +0300

    temp class

--- property.py
+++ property.py
@@ -2,16 +2,21 @@ class UserMail:
     def __init__(self,login,email):
         self.login=login
         self.__email=email
-    @property
-    def email(self):
+    def get_email(self):
         return self.__email
-    @email.setter
-    def email(self,new_email):
+    def set_email(self,new_email):
         a=new_email.count('@')
         if a==1:
             c=new_email.find('@')
             if '.' in new_email[c+1:]:
                 self.__email=new_email
         else:
-            print("Wrong  Email")
+            print("Ошибочная почта")
 
+    email=property(fget=get_email,fset=set_email)
+k = UserMail('belosnezhka', 'prince@wait.you')
+print(k.email)  # prince@wait.you
+k.email = [1, 2, 3] # Ошибочная почта
+k.email = 'prince@still@.wait'  # Ошибочная почта
+k.email = 'prince@still.wait'
+print(k.email)  # prince@still.wait
